aa4805005195683e472f949901e89885d6ce2219
403334 <ri.P04078@jsol.co.jp>
2020-06-29 08:38:33 +0000

    First Version

---
+++ webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function (AppComponent) {
+	return AppComponent.extend("ZJRI44P14_L0130.Component", {
+		metadata: {
+			"manifest": "json"
+		}
+	});
+});
---
+++ webapp/ext/controller/ListReportExt.controller.js
@@ -0,0 +1,71 @@
+sap.ui.controller("ZJRI44P14_L0130.ext.controller.ListReportExt", {
+	// 半年以内
+	onClickActionxJRIx44P141003CS1: function (oEvent) {
+		//Current日付
+		var curDate = (new Date()).getTime();
+		//半年（秒単位）
+		var halfYear = 365 / 2 * 24 * 3600 * 1000;
+		//半年前を計算する
+		var pastResult = curDate - halfYear;
+		var pastDate = new Date(pastResult);
+		var dd = pastDate.getDate();
+		var mm = pastDate.getMonth() + 1;
+		var yyyy = pastDate.getFullYear();
+
+		if (dd < 10) {
+			dd = '0' + dd;
+		}
+		if (mm < 10) {
+			mm = '0' + mm;
+		}
+		var DisplayDay = yyyy + '/' + mm + '/' + dd;
+
+		var oSmartFilter = this.getView().byId(
+			"ZJRI44P14_L0130::sap.suite.ui.generic.template.ListReport.view.ListReport::xJRIx44P141003CS--listReportFilter");
+		var oDefaultFilter = {
+			erdat: {
+				value: null,
+				items: [],
+				ranges: [{
+					"exclude": false,
+					"operation": "GT",
+					"keyField": "erdat",
+					"value1": DisplayDay,
+					"value2": null
+				}]
+			}
+		};
+		oSmartFilter.setFilterData(oDefaultFilter);
+	},
+	// 1年以内
+	onClickActionxJRIx44P141003CS2: function (oEvent) {
+		var today = new Date();
+		var dd = today.getDate();
+		var mm = today.getMonth() + 1;
+		var yyyy = today.getFullYear() - 1;
+
+		if (dd < 10) {
+			dd = '0' + dd;
+		}
+		if (mm < 10) {
+			mm = '0' + mm;
+		}
+		var DisplayDay = yyyy + '/' + mm + '/' + dd;
+		var oSmartFilter = this.getView().byId(
+			"ZJRI44P14_L0130::sap.suite.ui.generic.template.ListReport.view.ListReport::xJRIx44P141003CS--listReportFilter");
+		var oDefaultFilter = {
+			erdat: {
+				value: null,
+				items: [],
+				ranges: [{
+					"exclude": false,
+					"operation": "GT",
+					"keyField": "erdat",
+					"value1": DisplayDay,
+					"value2": null
+				}]
+			}
+		};
+		oSmartFilter.setFilterData(oDefaultFilter);
+	}
+});
